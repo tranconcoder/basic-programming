0ec02c738bf0ea8dfb4d88e3b84528e7b1cd2036
tranconcoder <tranconcoder@gmail.com>
2023-01-14 07:18:24 +0700

    complete part 3

---
+++ Chuong 3 - Du lieu kieu Mang?/10.cpp
@@ -0,0 +1,3 @@
+// Nhập 2 ma trận kiểu số thực
+// Kiểm tra 2 ma trận tính tích được không
+// Chả hiểu đề lun ma trận nào chả tính tích được
---
+++ Chuong 3 - Du lieu kieu Mang?/lib.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print_arr(vector<int>);
+void print_arr(vector<double>);
+
+void print_arr(vector<int>) {
+
+}
